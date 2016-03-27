924398d8877b5d0ee079e6f12d4ad81a826d9cf7
Jez Cope <j.cope@erambler.co.uk>
2016-03-27 21:28:09 +0100

    Tweet blog posts now and later
    Tweet now using teaser (if present) and schedule a tweet for later using
    the title

--- scripts/erambler_syndicate.py
+++ scripts/erambler_syndicate.py
@@ -5,17 +5,20 @@ import yaml
 import requests
 
 class BufferSyndicate:
-    def __init__(self, config):
+    def __init__(self, config, now=False, format_string='Recently blogged: {title}'):
         self.config = config['buffer']
+        self.now = now
+        self.format_string = format_string
 
-    def syndicate(self, text, url):
-        print('Sending to buffer: "{} {}"... '.format(text, url), end='', flush=True)
+    def syndicate(self, title, teaser, url):
+        text = self.format_string.format(title=title, teaser=teaser)
+
+        print('Sending to buffer: "{} {}"... '.format(text, url), end='\n', flush=True)
 
         r = requests.post('https://api.bufferapp.com/1/updates/create.json',
                           data={
                               'access_token': self.config['access_token'],
-                              'profile_ids[]': [self.config['tw_profile'],
-                                                self.config['fb_profile']],
+                              'profile_ids[]': [self.config['tw_profile']],
                               'text': text,
                               'media[link]': url
                               })
@@ -25,9 +28,6 @@ class BufferSyndicate:
         else:
             print('Failed with code {}'.format(r.status_code))
             print('Error:', r.json()['message'])
-
-class TweetNowSyndicate:
-    pass
 
 class EmailNotifySyndicate:
     pass
@@ -43,7 +43,8 @@ except:
 with open('plumbing.yaml') as f:
     config = yaml.load(f)
 
-syndicate = [BufferSyndicate(config)]
+syndicate = [BufferSyndicate(config),
+             BufferSyndicate(config, now=True, format_string='{teaser}')]
 
 last_updated = state.get('er_last_updated', dt(2000, 1, 2))
 
@@ -51,7 +52,9 @@ for entry in feed.entries:
     entry_updated = dt.fromtimestamp(mktime(entry.updated_parsed))
     if entry_updated > last_updated:
         for s in syndicate:
-            s.syndicate('Recently blogged: ' + entry.title, entry.link)
+            s.syndicate(title=entry.title,
+                        teaser=entry.get('prism_teaser', entry.title),
+                        url=entry.link)
 
 state['er_last_updated'] = dt.now()
 yaml.dump(state, open('plumbing-state.yaml', 'w'), default_flow_style=False)
